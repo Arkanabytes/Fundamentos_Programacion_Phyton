a15d9f518cf7963c93d8320c620cba14084d9585
Alejandra Pinto <cpinto36@alumno.uned.es>
2025-04-02 21:23:31 -0300

    Consulta_edad.py

---
+++ Consulta_edad.py
@@ -0,0 +1,9 @@
+# Consultad edad
+
+edad= int (input ('Cual es tu edad '))
+if edad >= 18:
+    print ('Eres mayor de edad')
+else :
+    print ('Eres menor de edad')
+
+print ('Programa terminado')
