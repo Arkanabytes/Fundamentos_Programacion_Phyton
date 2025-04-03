9d10689b23a27a658df859e4dc16ed7cc1dc9b7f
Alejandra Pinto <cpinto36@alumno.uned.es>
2025-04-02 21:18:42 -0300

    calificacion_alumno.py

---
+++ calificacion_alumno.py
@@ -0,0 +1,11 @@
+# Calificacion Alummno
+
+calificacion = float (input('Ingrese la calificacion de un Alummno '))
+if calificacion >= 6.0:
+    print ('Felicitaciones, estas eximido, ya que tu calificacion es igual o superior a a 6,0!!')
+elif calificacion >= 4.0:
+    print ('Felicitaciones, estas eximido, ya que tu calificacion es igual o superior a a 4,0!!')
+else:
+    print ('¡Lo siento, reprobaste, tu calificacion es igual o inferior a 4,0¡')
+    
+print ('Autor: Consuelo Pinto')
