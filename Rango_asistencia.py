7ce168c13df0574dec1daebc7e6d9443eee81e35
Alejandra Pinto <cpinto36@alumno.uned.es>
2025-04-02 21:20:17 -0300

    Rango_asistencia.py

---
+++ Rango_asistencia.py
@@ -0,0 +1,13 @@
+# Rango de asistencia
+
+asistencia = int (input('Cual es tu promedio de asistencia '))
+if asistencia >= 100:
+    print ('Tu asistencia es completa vas bien!!')
+elif asistencia >= 70:
+    print ('Tu asistencia es relativamente buena!!')
+elif asistencia >= 50:
+    print ('Tu asistencia es de la mitad NO FALTES MAS')
+else:
+    print ('TU ASISTENCIA ES MALISIMA DEBES IR A DIRECCION')
+    
+print ('Autor: Consuelo Pinto')
