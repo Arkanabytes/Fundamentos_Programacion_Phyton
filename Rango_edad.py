faffffb321f6618ff025af8ebe2f33c8d0a9d40e
Alejandra Pinto <cpinto36@alumno.uned.es>
2025-04-02 21:22:04 -0300

    Rango_edad.py

---
+++ Rango_edad.py
@@ -0,0 +1,12 @@
+#Rango edad
+edad= int(input ('Cual es tu edad? '))
+if edad >= 18:
+    print ('Eres mayor de edad')
+elif edad >= 10:
+    print ('Eres un Preadolecente')
+elif edad >= 5:
+    print ('Eres un niño')
+else:
+    print ('Eres un bebe')
+    
+    
