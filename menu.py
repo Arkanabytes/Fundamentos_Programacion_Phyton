c6723ee96364cf5aa613892d94db4354d447f984
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-04-09 22:04:27 -0400

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,18 +1,22 @@
-# Menu con while True
-while True:
-    print('\n Menu')
-    print('1.- Menu')
-    print('2.- Pregunta edad')
-    print('3.- Salir')
 
-    opcion= input('Elige una opcion 1/2/3')
-    if opcion == '1':
-        print ('Holaaaaaa!!')
-    elif opcion == '2':
-        edad=int(input('Cual es su edad'))
-         print (f'Tienes {edad} años')
-        elif opcion == '3':
-         print ('Hasta luego')
-         break
-     else
         
+    #Menu con while True
+
+while True:
+    print('\n Menú:')
+    print('1-. Saludar')
+    print('2-. Pregunta edad')
+    print('3-. Salir')
+
+    opcion=input('Elige una opcion 1/2/3:')
+
+    if opcion == '1':
+        print( 'Holaaaa!!!')
+    elif opcion == '2':
+        edad=int(input('¿Cual es tu edad?'))
+        print (f'Tienes {edad} años')
+    elif opcion == '3':
+        print ('Hasta luego, chao')
+        break
+    else:
+        print('Opcion no válida, intente de nuevo')
