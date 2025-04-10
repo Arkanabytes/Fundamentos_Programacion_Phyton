d39ebfe401ad50846063c968767d9230d74c4917
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-04-09 21:17:13 -0400

    Add files via upload

---
+++ listasFrutas.py
@@ -0,0 +1,24 @@
+# Listas
+frutas=['Manzana', 'Pera','Mango']
+
+# indice pare en 0 y cada valor recibe el primer elemento
+
+# acceder a los elementos
+print(frutas)
+print (frutas[0]) # Manzana
+print (frutas[2]) # Pera
+
+# modificar elementos de la lista
+frutas [1] = 'kiwi'
+print(frutas) # manzana, kiwi, mango
+
+# metodos utiles para modificar una lista
+frutas.append('Uva') # agrega
+print(frutas) # ['Manzana', 'Pera','Mango', 'Uva']
+
+frutas.remove('Manzana') # agrega
+print(frutas) # ['kiwi', 'Pera', 'Uva']
+
+print(len(frutas))# cantidad de elementos
+frutas.sort() #ordenar alfabeticamente
+print(frutas)
