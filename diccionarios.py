e982636fcebcafd2feb1d3455327864422c0df70
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-04-16 19:03:59 -0400

    Add files via upload

---
+++ diccionarios.py
@@ -0,0 +1,24 @@
+# Diccionarios
+
+persona ={ 'nombre': 'Ana', 'edad': 30}
+print (persona['nombre']) # Ana
+print (persona)
+persona = {
+    'nombre': 'ana',
+    'edad': 30,
+    'ciudad': 'santiago'
+}
+
+# Crear diccionario desde cero
+print (persona['nombre'])
+print (persona['ciudad'])
+
+producto= {} #Producto vacio
+producto ['nombre'] = 'Mouse'
+producto ['precio'] = 12000
+producto ['stock'] = 25
+
+print(producto)
+
+for clave in producto:
+    print(clave, '->', producto [clave])
