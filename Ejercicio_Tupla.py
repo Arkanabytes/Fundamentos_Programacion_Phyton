f79984186061178184660a187143a1c4d8ef74fd
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-04-16 21:49:09 -0400

    Add files via upload

---
+++ Ejercicio_Tupla.py
@@ -0,0 +1,15 @@
+# Crea una tupla 
+# con sus datos nombre, edad, signo
+# imprima con f- strings la siguiente 
+
+# crear 
+mi_tupla=()
+
+# agregar datos
+mi_tupla= ('Consuelo', 35, 'Capricornio')
+
+# desempaquetar
+nombre, edad, signo = mi_tupla
+
+#Imprime mensaje
+print(f'Me llamo {nombre}, tengo {edad} y mi signo es {signo}')
