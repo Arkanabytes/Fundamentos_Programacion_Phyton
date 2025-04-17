1e9ed9202f6ec3f0a6eccf2a5d886b9abf84b76a
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-04-16 21:32:36 -0400

    Add files via upload

---
+++ Ensayo_listas.py
@@ -0,0 +1,15 @@
+# Crea una lista vacia que se llame nombres
+# Permite al usuario ingresar nombre en bucle while hasta que escriba fin, luego 
+# agrega cada nombre a la lista e imprimelos (numerados) usando el bucle for
+
+# Listas
+nombres =[]
+
+while True:
+    nombre= input('Ingrese su nombre o escriba "Fin" para salir ')
+    if nombre == 'fin'or'FIN':
+        break
+    nombres.append(nombre)
+print('Lista de nombres ingresados')
+for i, nombre in enumerate(nombres, start=1):
+    print(f'{i}-.{nombre}')
