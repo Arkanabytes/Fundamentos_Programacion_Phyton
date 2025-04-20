4bfdbbdd8478484425f4ebf331748bc8d4bf47c7
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-04-20 17:23:20 -0400

    Add files via upload

---
+++ Ensayo_tupla_fecha.py
@@ -0,0 +1,14 @@
+# Crea un tupla  llamada'fecha' que contenga el dia mes año de hoy. Desempaqueta la tupla
+# y muestra le fecha  en el formato hoy es el dia 20 de mes de abril y del año 2025
+# Crea tupla
+fecha = ()
+
+# Que contienen la tupla
+fecha = ( 20, 'abril', 2025)
+
+# Desempaqueta la tupla
+dia, mes, anio = fecha
+
+# imprime los datos de la tupla
+print (f'Hoy es el {dia} del {mes} del año {anio}')
+
