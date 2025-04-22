5ab843ae70abef9156014c0a87e93b72f990580c
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-04-22 19:07:16 -0400

    Add files via upload

---
+++ Ensayo_Profe_Nicol.py
@@ -0,0 +1,14 @@
+# Crea que ingrese dos numeros  y  que la division sea el primero por el segundo
+
+while True:
+    try:
+        num1= int(input ('Ingrese el primero numero: '))
+        num2= int(input ('Ingrese el primero numero: '))
+        
+        resultado = num1/num2
+        print (f' El resultado de la division es: {resultado} ')
+        break
+    except ValueError:
+        print('Error: Numero no valido')
+    except ValueError:
+        print('Error: Numero no se puede dividir por cero')
